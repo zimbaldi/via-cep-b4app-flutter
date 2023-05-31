30193cac9e5073899d428222da29782c734ea23e
zimbaldi <italo.zimbaldi@yahoo.com.br>
2023-05-31 18:44:40 -0300

    title name changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'ViaCep DB App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
